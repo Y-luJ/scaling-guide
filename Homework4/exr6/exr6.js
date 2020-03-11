674703cbf74f4b98696b0ef06c981a5e14f7fb12
Y-luJ <juss11@mail.ru>
2020-03-11 21:18:08 +0300

    Update exr6.js

--- Homework4/exr6/exr6.js
+++ Homework4/exr6/exr6.js
@@ -10,20 +10,52 @@ if (x>0) {
         document.getElementById("answer").innerHTML = answer;
     }
     else {
-        console.log('Точка ('+x+','+y+') принадлежит четвертой четверти');
-        var answer= 'Точка ('+x+','+y+') принадлежит четвертой четверти';
-        document.getElementById("answer").innerHTML = answer;
+        if (y<0) {
+            console.log('Точка ('+x+','+y+') принадлежит четвертой четверти');
+            var answer= 'Точка ('+x+','+y+') принадлежит четвертой четверти';
+            document.getElementById("answer").innerHTML = answer;
+        }
+        else {
+            console.log('Точка ('+x+','+y+') лежит на оси абсцисс в правой полуплоскости');
+            var answer= 'Точка ('+x+','+y+') лежит на оси абсцисс в правой полуплоскости';
+            document.getElementById("answer").innerHTML = answer;
+        }
+        
     }  
 }
-else {
+else if (x<0) {
     if (y>0) {
         console.log('Точка ('+x+','+y+') принадлежит второй четверти');
         var answer= 'Точка ('+x+','+y+') принадлежит второй четверти';
         document.getElementById("answer").innerHTML = answer;
     }
     else {
-        console.log('Точка ('+x+','+y+') принадлежит третьей четверти');
-        var answer= 'Точка ('+x+','+y+') принадлежит третьей четверти';
+        if (y<0) {
+            console.log('Точка ('+x+','+y+') принадлежит третьей четверти');
+            var answer= 'Точка ('+x+','+y+') принадлежит третьей четверти';
+            document.getElementById("answer").innerHTML = answer;
+        }
+        else {
+            console.log('Точка ('+x+','+y+') лежит на оси абсцисс в левой полуплоскости');
+            var answer= 'Точка ('+x+','+y+') лежит на оси абсцисс в левой полуплоскости';
+            document.getElementById("answer").innerHTML = answer;
+        }
+    }
+}
+else {
+    if (y>0) {
+        console.log('Точка ('+x+','+y+') лежит на оси ординат в верхней полуплоскости');
+        var answer= 'Точка ('+x+','+y+') лежит на оси абсцисс в верхней полуплоскости';
+        document.getElementById("answer").innerHTML = answer;
+        }
+    else if (y<0) {
+        console.log('Точка ('+x+','+y+') лежит на оси ординат в нижней полуплоскости');
+        var answer= 'Точка ('+x+','+y+') лежит на оси абсцисс в нижней полуплоскости';
+        document.getElementById("answer").innerHTML = answer;
+        }
+    else {
+        console.log('Поздравляю! Вы в центре координат!');
+        var answer= 'Поздравляю! Вы в центре координат!';
         document.getElementById("answer").innerHTML = answer;
     }
 }
